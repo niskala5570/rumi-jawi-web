b0decea168ab8be8498f6eaebaa883328da9ceb6
Niskala Airaha <34799053+niskala5570@users.noreply.github.com>
2024-04-23 12:19:36 +0800

    Kemas kini data dan juga kod menjananya

--- make-js-extended.py
+++ make-js-extended.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+# This code save as long array, easy on eyes
 
 import sys
 
@@ -6,14 +7,15 @@ if len(sys.argv) != 2:
     sys.exit('usage: make-js.py laluan/ke/rumi-jawi-tambahan.tsv') 
 tsvpath = sys.argv[1]
 
-with open('static/rumi-jawi-tambahan.js', 'w') as out:
-    with open(tsvpath) as f:
+with open('static/rumi-jawi-tambahan.js', 'w', encoding='utf-8') as out:
+    with open(tsvpath, encoding='utf-8') as f:
         next(f)  # discard header
-        parts = [line.rstrip() for line in f]
-        data = '\\n'.join(parts).replace('"', '')
-        print(f'var rjtambahan = "{data}";', file=out)
+        data = [line.rstrip().replace('"', '') for line in f]
+        print('var rjtambahan = [', file=out)
+        print(',\n'.join([f'  "{line}"' for line in data]), file=out)
+        print('];', file=out)
     print('''
-rjtambahan.split("\\n").forEach(function (line) {
+rjtambahan.forEach(function (line) {
   let parts = line.split("\\t");
   let rumi = parts[0];
   let normRumi = rumi.toLowerCase();
@@ -24,3 +26,34 @@ rjtambahan.split("\\n").forEach(function (line) {
   jrmap.get(jawi).push(rumi);
 })
 ''', file=out)
+    
+
+# This save the data .js file as long string,
+# Comment the code above to use the code below.
+
+
+# import sys
+
+# if len(sys.argv) != 2:
+#     sys.exit('usage: make-js.py laluan/ke/rumi-jawi-tambahan.tsv') 
+# tsvpath = sys.argv[1]
+
+# with open('static/rumi-jawi-tambahan.js', 'w') as out:
+#     with open(tsvpath) as f:
+#         next(f)  # discard header
+#         parts = [line.rstrip() for line in f]
+#         data = '\\n'.join(parts).replace('"', '')
+#         print(f'var rjtambahan = "{data}";', file=out)
+#     print('''
+# rjtambahan.split("\\n").forEach(function (line) {
+#   let parts = line.split("\\t");
+#   let rumi = parts[0];
+#   let normRumi = rumi.toLowerCase();
+#   let jawi = parts[1];
+#   if (!rjmap.has(normRumi)) rjmap.set(normRumi, []);
+#   rjmap.get(normRumi).push(jawi);
+#   if (!jrmap.has(jawi)) jrmap.set(jawi, []);
+#   jrmap.get(jawi).push(rumi);
+# })
+# ''', file=out)
+
